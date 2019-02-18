87f8870a2670af2ccc2b4eac03474229b3b955e0
RichardFuentes369 <javierbaron6@gmail.com>
2019-02-17 21:14:43 -0500

    DATOS SEEDER

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -77,99 +77,180 @@ class UsersSeeder extends Seeder
         $user4->save();
 
         $user5 = User::create([
-           'id'        => 5,
-           'name'      => 'Mark',
-           'lastname'      => 'Zuckerberg Sinisterra',
-           'email' => 'prueba5@gmail.com',
-           'birthdate' => '1996-06-01',
-           'dni' => '55555555',
-           'sexo' => 'M',
-           'category' => 'alumno',
-           'phone' => '6390852',
-           'cellphone' => '3203687952',
-           'colegio_id' => 1,
-           ]);
+            'id'        => 5,
+            'name'      => 'Esperanza',
+            'lastname'      => 'Fuentes Porras',
+            'email' => 'prueba5@gmail.com',
+            'birthdate' => '1996-06-01',
+            'dni' => '55555555',
+            'sexo' => 'F',
+            'category' => 'profesor',
+            'phone' => '6588799',
+            'cellphone' => '3168574889',
+            'colegio_id' => 1,
+            ]);
 
         $user5->save();
 
-        $user10 = User::create([
-           'id'        => 10,
-           'name'      => 'Alan',
-           'lastname'      => 'Turing',
-           'email' => 'prueba10@gmail.com',
-           'birthdate' => '1996-06-01',
-           'dni' => '10101010',
-           'sexo' => 'M',
-           'category' => 'alumno',
-           'phone' => '6390852',
-           'cellphone' => '3203687952',
-           'colegio_id' => 1,
-           ]);
-
-        $user10->save();
-
         $user6 = User::create([
            'id'        => 6,
-           'name'      => 'Eliot',
-           'lastname'      => 'Bautista Jaimes',
+           'name'      => 'Mark',
+           'lastname'      => 'Rodriguez Molano',
            'email' => 'prueba6@gmail.com',
            'birthdate' => '1996-06-01',
            'dni' => '66666666',
            'sexo' => 'M',
-           'category' => 'rector',
-           'phone' => '6852200',
-           'cellphone' => '3208756325',
-           'colegio_id' => 2,
+           'category' => 'alumno',
+           'phone' => '6390852',
+           'cellphone' => '3203687952',
+           'colegio_id' => 1,
            ]);
 
         $user6->save();
 
         $user7 = User::create([
            'id'        => 7,
-           'name'      => 'Henry',
-           'lastname'      => 'Ford Mustang',
+           'name'      => 'Alan',
+           'lastname'      => 'Turing',
            'email' => 'prueba7@gmail.com',
            'birthdate' => '1996-06-01',
            'dni' => '77777777',
            'sexo' => 'M',
-           'category' => 'secretario',
-           'phone' => '6395893',
-           'cellphone' => '3203658964',
+           'category' => 'alumno',
+           'phone' => '6390852',
+           'cellphone' => '3203687952',
            'colegio_id' => 1,
            ]);
 
         $user7->save();
 
-         $user8 = User::create([
-            'id'        => 8,
-            'name'      => 'Dennis',
-            'lastname'      => 'Buenaventura Solis',
-            'email' => 'prueba8@gmail.com',
-            'birthdate' => '1996-06-01',
-            'dni' => '88888888',
-            'sexo' => 'M',
-            'category' => 'profesor',
-            'phone' => '6632008',
-            'cellphone' => '3013256964',
-            'colegio_id' => 1,
-            ]);
+        $user8 = User::create([
+           'id'        => 8,
+           'name'      => 'Eliot',
+           'lastname'      => 'Bautista Jaimes',
+           'email' => 'prueba8@gmail.com',
+           'birthdate' => '1996-06-01',
+           'dni' => '88888888',
+           'sexo' => 'M',
+           'category' => 'alumno',
+           'phone' => '6852200',
+           'cellphone' => '3208756325',
+           'colegio_id' => 1,
+           ]);
 
         $user8->save();
 
         $user9 = User::create([
            'id'        => 9,
-           'name'      => 'Felix',
-           'lastname'      => 'Quintero Agames',
+           'name'      => 'Enrique',
+           'lastname'      => 'Velez Cifuentes',
            'email' => 'prueba9@gmail.com',
            'birthdate' => '1996-06-01',
            'dni' => '99999999',
            'sexo' => 'M',
-           'category' => 'alumno',
-           'phone' => '6390000',
-           'cellphone' => '3203589979',
-           'colegio_id' => 1,
+           'category' => 'rector',
+           'phone' => '6387452',
+           'cellphone' => '3209687854',
+           'colegio_id' => 2,
            ]);
 
         $user9->save();
+
+        $user10 = User::create([
+           'id'        => 10,
+           'name'      => 'Samuel',
+           'lastname'      => 'Ortiz Moreno',
+           'email' => 'prueba10@gmail.com',
+           'birthdate' => '1996-06-01',
+           'dni' => '10101010',
+           'sexo' => 'M',
+           'category' => 'secretario',
+           'phone' => '6578423',
+           'cellphone' => '3112548761',
+           'colegio_id' => 2,
+           ]);
+
+        $user10->save();
+
+         $user11 = User::create([
+            'id'        => 11,
+            'name'      => 'Salome',
+            'lastname'      => 'Camargo Solano',
+            'email' => 'prueba11@gmail.com',
+            'birthdate' => '1996-06-01',
+            'dni' => '01010101',
+            'sexo' => 'F',
+            'category' => 'profesor',
+            'phone' => '6588799',
+            'cellphone' => '3168574889',
+            'colegio_id' => 2,
+            ]);
+
+        $user11->save();
+
+        $user12 = User::create([
+            'id'        => 12,
+            'name'      => 'Rebeca',
+            'lastname'      => 'Rojas Porras',
+            'email' => 'prueba12@gmail.com',
+            'birthdate' => '1996-06-01',
+            'dni' => '12121212',
+            'sexo' => 'F',
+            'category' => 'profesor',
+            'phone' => '6588799',
+            'cellphone' => '3168574889',
+            'colegio_id' => 2,
+            ]);
+
+        $user12->save();
+
+        $user13 = User::create([
+           'id'        => 13,
+           'name'      => 'Dulio',
+           'lastname'      => 'Palacios Sinisterra',
+           'email' => 'prueba13@gmail.com',
+           'birthdate' => '1996-06-01',
+           'dni' => '13131313',
+           'sexo' => 'M',
+           'category' => 'alumno',
+           'phone' => '6390852',
+           'cellphone' => '3203687952',
+           'colegio_id' => 2,
+           ]);
+
+        $user13->save();
+
+        $user14 = User::create([
+           'id'        => 14,
+           'name'      => 'Gilgerto',
+           'lastname'      => 'Santarrosa',
+           'email' => 'prueba14@gmail.com',
+           'birthdate' => '1996-06-01',
+           'dni' => '14141414',
+           'sexo' => 'M',
+           'category' => 'alumno',
+           'phone' => '6390852',
+           'cellphone' => '3203687952',
+           'colegio_id' => 2,
+           ]);
+
+        $user14->save();
+
+        $user15 = User::create([
+           'id'        => 15,
+           'name'      => 'Dennis',
+           'lastname'      => 'Jaimes Palacios',
+           'email' => 'prueba15@gmail.com',
+           'birthdate' => '1996-06-01',
+           'dni' => '15151515',
+           'sexo' => 'M',
+           'category' => 'alumno',
+           'phone' => '6852200',
+           'cellphone' => '3208756325',
+           'colegio_id' => 2,
+           ]);
+
+        $user15->save();
+
     }
 }
